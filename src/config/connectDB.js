8dbb97a905c1226d5becacdef47395e39abd08a8
lokeshvaishnav11 <lokeshkumariit@gmail.com>
2026-02-16 14:49:53 +0530

    FGHJK

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -15,21 +15,21 @@ const mysql = require('mysql2/promise');
 
 // const mysql = require('mysql2/promise');
 
-// const connection = mysql.createPool({
-//   host: '127.0.0.1',  // or '127.0.0.1'
-//   user: 'avaitorgame',
-//   password: 'Avatior123',
-//   database: 'gamedb'
-// });
+const connection = mysql.createPool({
+  host: '127.0.0.1',  // or '127.0.0.1'
+  user: 'avaitorgame',
+  password: 'Avatior123',
+  database: 'gamedb'
+});
 
 // #1D4268
 
-const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',  // XAMPP's default root password is blank
-    database: 'gameavitor',
-    port: 3306
-});
+// const connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',  // XAMPP's default root password is blank
+//     database: 'gameavitor',
+//     port: 3306
+// });
 
 module.exports = connection;
